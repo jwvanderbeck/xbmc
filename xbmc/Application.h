4582e8283a12a20fab2ecc3cee77eff223c4177d
Memphiz <memphis@machzwo.de>
2015-12-21 12:03:13 +0100

    [application/darwin] - cleanup of platform dependend directory setup - added tvos support in there too

--- xbmc/Application.h
+++ xbmc/Application.h
@@ -503,7 +503,7 @@ protected:
   float NavigationIdleTime();
 
   bool InitDirectoriesLinux();
-  bool InitDirectoriesOSX();
+  bool InitDirectoriesDarwin();
   bool InitDirectoriesWin32();
   void CreateUserDirs() const;
 

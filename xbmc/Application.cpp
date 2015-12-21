274eaffad36efb9332a4a4a8e884bcd4e9dfd612
Memphiz <memphis@machzwo.de>
2015-12-21 12:03:42 +0100

    [application/tvos] - use os screensaver support on this platform

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -3548,7 +3548,7 @@ void CApplication::OnPlayBackEnded()
 #endif
 #ifdef TARGET_ANDROID
   CXBMCApp::OnPlayBackEnded();
-#elif defined(TARGET_DARWIN_IOS)
+#elif defined(TARGET_DARWIN_TVOS)
   CDarwinUtils::EnableOSScreenSaver(true);
 #endif
 
@@ -3575,8 +3575,8 @@ void CApplication::OnPlayBackStarted()
 #endif
 #ifdef TARGET_ANDROID
   CXBMCApp::OnPlayBackStarted();
-#elif defined(TARGET_DARWIN_IOS)
-  if (m_pPlayer->IsPlayingVideo())
+#elif defined(TARGET_DARWIN_TVOS)
+  if (!m_pPlayer->IsPlayingAudio())
     CDarwinUtils::EnableOSScreenSaver(false);
 #endif
 
@@ -3615,7 +3615,7 @@ void CApplication::OnPlayBackStopped()
 #endif
 #ifdef TARGET_ANDROID
   CXBMCApp::OnPlayBackStopped();
-#elif defined(TARGET_DARWIN_IOS)
+#elif defined(TARGET_DARWIN_TVOS)
   CDarwinUtils::EnableOSScreenSaver(true);
 #endif
 
@@ -3634,7 +3634,7 @@ void CApplication::OnPlayBackPaused()
 #endif
 #ifdef TARGET_ANDROID
   CXBMCApp::OnPlayBackPaused();
-#elif defined(TARGET_DARWIN_IOS)
+#elif defined(TARGET_DARWIN_TVOS)
   CDarwinUtils::EnableOSScreenSaver(true);
 #endif
 
@@ -3651,8 +3651,7 @@ void CApplication::OnPlayBackResumed()
 #endif
 #ifdef TARGET_ANDROID
   CXBMCApp::OnPlayBackResumed();
-#elif defined(TARGET_DARWIN_IOS)
-  if (m_pPlayer->IsPlayingVideo())
+  if (!m_pPlayer->IsPlayingAudio())
     CDarwinUtils::EnableOSScreenSaver(false);
 #endif
 
@@ -3917,6 +3916,10 @@ bool CApplication::WakeUpScreenSaverAndDPMS(bool bPowerOffKeyPressed /* = false 
   else
     result = WakeUpScreenSaver(bPowerOffKeyPressed);
 
+#if defined(TARGET_DARWIN_TVOS)
+  result = CDarwinUtils::ResetSystemIdleTimer();
+#endif
+
   if(result)
   {
     // allow listeners to ignore the deactivation if it preceeds a powerdown/suspend etc
@@ -3928,7 +3931,6 @@ bool CApplication::WakeUpScreenSaverAndDPMS(bool bPowerOffKeyPressed /* = false 
     CXBMCApp::EnableWakeLock(true);
 #endif
   }
-
   return result;
 }
 
